e38138dffbf54cdadd9844fac3c12a4ee4e3f93b
kjunn2000 <kjunn2000@gmail.com>
2023-01-21 01:49:44 +0800

    first commit

---
+++ src/main/java/com/kjunn/fineye/FineyeApplication.java
@@ -0,0 +1,13 @@
+package com.kjunn.fineye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FineyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjunn/fineye/FineyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjunn.fineye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FineyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
